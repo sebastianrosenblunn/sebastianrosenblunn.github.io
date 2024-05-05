86df67df92ae1dff98ab6232b36fdd29ec855ba7
sebastian rosenblunn <108303078+sebastianrosenblunnhellman@users.noreply.github.com>
2024-05-05 19:29:20 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+function showSidebar() {
+  const sidebar = document.querySelector('.sidebar'); // Selecciona el elemento con la clase 'sidebar'
+  sidebar.style.display = 'flex'; // Cambia el estilo de visualización para mostrar la barra lateral como una fila flexible
+}
+
+function hideSidebar() {
+  const sidebar = document.querySelector('.sidebar'); // Selecciona el elemento con la clase 'sidebar'
+  sidebar.style.display = 'none'; // Oculta la barra lateral estableciendo su estilo de visualización en 'none'
+}
+
+
